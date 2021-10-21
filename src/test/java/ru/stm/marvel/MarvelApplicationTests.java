5aadc3d0562d42c54845d84c5210752aafc0c2db
prgshut <prgshut@gmail.com>
2021-10-22 00:30:05 +0300

    install

---
+++ src/test/java/ru/stm/marvel/MarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stm.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
